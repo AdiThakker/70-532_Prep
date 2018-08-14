7974dee6868266f6adfe70e139c55b7172fbdb35
Aditya D Thakker <aditya.thakker@gmail.com>
2018-08-14 08:07:28 -0400

    added cosmosdb project.

---
+++ CosmosDbApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Documents.Client;
+using System;
+
+namespace CosmosDbApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");            
+        }
+    }
+}
