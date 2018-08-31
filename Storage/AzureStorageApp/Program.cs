a6996cd816a199b8decf5da99ce4e6ef19d8825d
Aditya D Thakker <aditya.thakker@gmail.com>
2018-08-30 21:05:28 -0400

    Basic queue operations

--- Storage/AzureStorageApp/Program.cs
+++ Storage/AzureStorageApp/Program.cs
@@ -14,7 +14,18 @@ namespace AzureStorageApp
         public static void Main(string[] args)
         {
             Console.WriteLine(GetConnectionString());
-            var result = CreateQueue("532squeue").Result;
+
+            // Queue 
+            if (!QueueOperations("532squeue").Result)
+                return;
+
+            // Table 
+
+            // Blob
+
+
+            // File
+
             Console.ReadKey();
         }
 
@@ -23,14 +34,45 @@ namespace AzureStorageApp
             return $"DefaultEndpointsProtocol=https;AccountName={accountName};AccountKey={accountKey};";
         }
 
-        private static async Task<bool> CreateQueue(string queueName)
+        private static async Task<bool> QueueOperations(string queueName)
         {
             try
             {
                 storageAccount = CloudStorageAccount.Parse(GetConnectionString());
                 var queueClient = storageAccount.CreateCloudQueueClient();
                 var queue = queueClient.GetQueueReference(queueName);
+
+                // Create Queue
                 await queue.CreateIfNotExistsAsync();
+
+                // Clear Messages if any
+                await queue.ClearAsync();
+
+                // Add Message
+                await queue.AddMessageAsync(new CloudQueueMessage("Hello Azure Queue!"));
+
+                await queue.FetchAttributesAsync();
+
+                Console.WriteLine($"Approximate Messages: {queue.ApproximateMessageCount}");
+
+                // Peek Message
+                var message = await queue.PeekMessageAsync();
+                Console.WriteLine($"Peek Message: {message.ToString()}");
+
+                // Retrieve Message
+                message = await queue.GetMessageAsync();                    
+                Console.WriteLine($"Retrieve Message: {message.ToString()}");
+
+                // Delete to finish processing
+                await queue.DeleteMessageAsync(message);
+
+                await queue.FetchAttributesAsync();
+
+                Console.WriteLine($"Approximate Messages: {queue.ApproximateMessageCount}");
+
+                // Delete Queue
+                await queue.DeleteIfExistsAsync();
+
                 return true;
             }
             catch (Exception ex)
